1ac7355b300484c8f565dbfa2aa42a1ca2cf3b74
MhCanova <micaelahcanova@gmail.com>
2022-09-18 19:31:30 -0300

    Level Manager
    Bug (NullReferenceException: Object reference not set to an instance of an object Health.Die () (at Assets/Scripts/Health.cs:64))

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] float sceneLoadDelay = 2;
+    
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad("GameOver", sceneLoadDelay));
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting the game...");
+        Application.Quit();
+    }
+
+    IEnumerator WaitAndLoad(string sceneName, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
+}
